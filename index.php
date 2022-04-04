69e237d1049b231b3226d119cce5a9fd081b48be
Matthew Setter <matthew@matthewsetter.com>
2022-04-04 15:56:58 +0200

    Explicitly initialise the DI container with a Twig service
    This change explicitly initialises the DI container and adds a Twig
    service to it, so that the default route body can be created by
    rendering a template instead of adding a string to the response body's
    content.

--- index.php
+++ index.php
@@ -1,17 +1,28 @@
 <?php
 
+use DI\Container;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
 
 require __DIR__ . '/vendor/autoload.php';
 
+$container = new Container();
+
+$container->set('view', function() {
+    return Twig::create(__DIR__ . '/src/templates');
+});
+
+AppFactory::setContainer($container);
 $app = AppFactory::create();
+$app->add(TwigMiddleware::createFromContainer($app));
 
 $app->get('/', function (Request $request, Response $response, array $args) {
-    $response->getBody()->write("Hello World!");
-
-    return $response;
+    return $this
+        ->get('view')
+        ->render($response, 'index.html.twig');
 });
 
 $app->run();
